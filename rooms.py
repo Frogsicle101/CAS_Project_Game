249807df11f1203da087a5462edd22b0a305095c
Frogsicle101 <31726016+Frogsicle101@users.noreply.github.com>
2018-02-02 19:29:58 +0530

    Created new play method, and generally made things work better

--- rooms.py
+++ rooms.py
@@ -1,6 +1,7 @@
 class Room:
     def __init__(self, contained):
         self.contained = contained
+        self.open = False
 
     def introText(self):
         raise NotImplementedError
@@ -15,3 +16,11 @@ class Corridor(Room):
 class OtherRoom(Room):
     def introText(self):
         return """You are in some random other room"""
+
+class DoorRoom(Room):
+    def introText(self):
+        return """You are in a room with a large imposing door"""
+
+class WinRoom(Room):
+    def introText(self):
+        return """You win"""
--- world.py
+++ world.py
@@ -5,23 +5,41 @@ import interactive as lever
 def doThing():
     print("Hello")
 
+def play():
+    stillPlaying = True
+    x = 0
+    y = 0
+    while stillPlaying:
+        currentRoom = world[x][y]
+        print("\n\n-------\nYou are at {}, {}".format(x, y))
+        print(currentRoom.introText())
+
+        inputValid = False
+        while not inputValid:
+            entry = input(">>> ").lower()
+            if entry == "quit":
+                inputValid = True
+                stillPlaying = False
+            elif entry.split(" ")[0].lower() == "teleport":
+                try:
+                    x = int(entry.split(" ")[1])
+                    y = int(entry.split(" ")[2])
+                    inputValid = True
+                except IndexError():
+                    print("Out of range")
+            elif entry == "pull lever":
+                for object in currentRoom.getContained():
+                    if isinstance(object, lever.Lever):
+                        object.getAction()
+                        inputValid = True
+
+
+
+
 world = [
-    [tile.Corridor([]), tile.OtherRoom([])],
+    [tile.Corridor([]), tile.DoorRoom([])],
     [tile.Corridor([])],
     [tile.OtherRoom([lever.Lever(doThing)])]
     ]
 
-while True:
-    entry = input("0 or 1")
-    currentRoom = world[2][0]
-    if entry == "die":
-        break
-    elif entry == "pull lever":
-        for object in currentRoom.getContained():
-            if isinstance(object, lever.Lever):
-                print("Test")
-                object.getAction()
-    else:
-        entry = int(entry)
-        position = ()
-        print(world[entry].introText())
+play()
