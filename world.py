3f17da3f1974cef3ad5f810148c2bf7a2f3a1b12
Frogsicle101 <31726016+Frogsicle101@users.noreply.github.com>
2018-02-02 18:08:49 +0530

    implemented very basic framework for lever function

--- world.py
+++ world.py
@@ -1,15 +1,27 @@
 #The main game world
 import rooms as tile
+import interactive as lever
+
+def doThing():
+    print("Hello")
 
 world = [
-    [tile.Corridor([]), tile.OtherRoom([])]
-    [tile.Corridor([])]
-    [tile.OtherRoom(Lever())]
+    [tile.Corridor([]), tile.OtherRoom([])],
+    [tile.Corridor([])],
+    [tile.OtherRoom([lever.Lever(doThing)])]
     ]
 
 while True:
     entry = input("0 or 1")
+    currentRoom = world[2][0]
     if entry == "die":
         break
-    entry = int(entry)
-    print(world[entry].introText())
+    elif entry == "pull lever":
+        for object in currentRoom.getContained():
+            if isinstance(object, lever.Lever):
+                print("Test")
+                object.getAction()
+    else:
+        entry = int(entry)
+        position = ()
+        print(world[entry].introText())
