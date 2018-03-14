07259ae772f352ad60bb65f245821c742dfc45c0
Frogsicle101 <31726016+Frogsicle101@users.noreply.github.com>
2018-03-14 17:13:11 +0530

    cleared out old commented code

--- world.py
+++ world.py
@@ -22,44 +22,6 @@ def play():
             except ValueError:
                 print("Invalid Input\n\n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            """entry = input(">>> ").lower()
-            if entry == "quit":
-                inputValid = True
-                stillPlaying = False
-            elif entry.split(" ")[0].lower() == "teleport":
-                try:
-                    testX = int(entry.split(" ")[1])
-                    testY = int(entry.split(" ")[2])
-                    if world[testX][testY].getOpen():
-                        x = testX
-                        y = testY
-                        inputValid = True
-                    else:
-                        print("Room is locked")
-                except IndexError():
-                    print("Out of range")
-            elif entry == "pull lever":
-                for object in currentRoom.getContained():
-                    if isinstance(object, lever.Lever):
-                        object.getAction()
-                        inputValid = True"""
-
-
-
-
 world = [
     [tile.Corridor([]), tile.DoorRoom([]), tile.WinRoom()],
     [tile.Corridor([])],
