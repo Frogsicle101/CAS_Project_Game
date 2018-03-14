2149365b5049c62dafbc26122d58940b365e158a
Frogsicle101 <31726016+Frogsicle101@users.noreply.github.com>
2018-03-14 16:16:07 +0530

    added comments to Player class

--- players.py
+++ players.py
@@ -1,7 +1,6 @@
 #The class for the player character in the game.
 class Player:
     def __init__(self, health, position, inventory):
-        self.health = health
-        self.position = position
-        self.inventory = inventory
-    
+        self.health = health #int
+        self.position = position #list of two ints
+        self.inventory = inventory #list of item objects
