93c10bbca90cfa4fb3900ce15db39a2e6f072ad6
Frogsicle101 <31726016+Frogsicle101@users.noreply.github.com>
2018-04-10 16:26:28 +0530

    Added Weapons subclass to items class

--- items.py
+++ items.py
@@ -3,3 +3,7 @@ class Item:
     def __init__(self, name, value):
         self.name = name
         self.value = value
+class Weapon(Item):
+    def __init__(self, name, value, damage):
+        self.damage = damage
+        super().__init__(name, value)
--- test_python.py
+++ test_python.py
@@ -1,5 +1,4 @@
-from rooms import *
-#this is a file to test python
-print("Hello World")
-room1 = Room(0, 0)
-print(room1.getX())
+from items import *
+
+sword = Weapon("John", 5, 10)
+print(sword.damage)
