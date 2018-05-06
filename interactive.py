e857d83bb98a76db3a602a46b1685c0128df42ac
Frogsicle101 <31726016+Frogsicle101@users.noreply.github.com>
2018-05-06 12:46:26 +0530

    more testing

--- interactive.py
+++
@@ -1,5 +0,0 @@
-class Lever:
-    def __init__(self, action):
-        self.action = action
-    def getAction(self):
-        return self.action()
--- test_python.py
+++
@@ -1,4 +0,0 @@
-from items import *
-
-sword = Weapon("John", 5, 10)
-print(sword.damage)
