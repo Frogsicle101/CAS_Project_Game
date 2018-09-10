271c1aca8aec1180ef13174dc9498fae1c4b2715
Advik Goel <19agoel@aes.ac.in>
2018-09-10 23:08:52 +0530

    Just testing to see how to add some rooms. Not at all a significant update.

--- rooms.py
+++ rooms.py
@@ -18,6 +18,15 @@ class DoorRoom(Room):
     def introText(self):
         return """You are in a room with a large imposing door"""
 
+class IceRoom(Room):
+    def introText(self):
+        return """You seem to be outside, in a frozen tundra. But how can that be?"""
+
+class StrangeCorridor(Room):
+    def introText(self):
+        return """You are in a corridor, yet it seems that the walls shift in your peripheral vision."""
+
+
 class WinRoom(Room):
     def __init__(self, contained):
         super().__init__(contained)
